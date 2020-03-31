07441a79fb5b318781c4b7697a6d7f3b14df24cc
Szymon Bergner <szymon.bergner@gmail.com>
2020-03-31 17:03:16 +0200

    slice homepage to smaller compontents

---
+++ src/components/menu-item/MenuItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./MenuItem.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">Shop now</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
